abd8338e5d15aaec50ee1752b7c6a6eac5a38ab4
webstoryboy <webstupids@gmail.com>
2022-06-02 20:41:01 +0900

    2022.06.02

--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,47 @@
 import { createRouter, createWebHistory } from "vue-router";
-import HomeView from "../views/HomeView.vue";
+import MainView from "../views/MainView.vue";
+import AboutView from "../views/AboutView.vue";
+import ReferView from "../views/ReferView.vue";
+import YoutubeView from "../views/YoutubeView.vue";
+import MovieView from "../views/MovieView.vue";
+import ContactView from "../views/ContactView.vue";
+import PortfolioView from "../views/PortfolioView.vue";
 
 const routes = [
   {
     path: "/",
-    name: "home",
-    component: HomeView,
+    name: "Main",
+    component: MainView,
   },
   {
     path: "/about",
-    name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+    name: "About",
+    component: AboutView,
+  },
+  {
+    path: "/reference",
+    name: "Reference",
+    component: ReferView,
+  },
+  {
+    path: "/youtube",
+    name: "Youtube",
+    component: YoutubeView,
+  },
+  {
+    path: "/movie",
+    name: "Movie",
+    component: MovieView,
+  },
+  {
+    path: "/contact",
+    name: "Contact",
+    component: ContactView,
+  },
+  {
+    path: "/portfolio",
+    name: "Portfolio",
+    component: PortfolioView,
   },
 ];
 
---
+++ src/views/MainView.vue
@@ -0,0 +1,17 @@
+<template>
+  <HeaderCont />
+  <main id="main"></main>
+  <FooterCont />
+</template>
+
+<script>
+import HeaderCont from "@/components/HeaderCont.vue";
+import FooterCont from "@/components/FooterCont.vue";
+
+export default {
+  components: {
+    HeaderCont,
+    FooterCont,
+  },
+};
+</script>
